f758da83129b03f1c3b5a9fc6ef2177c36f5410c
John Barson <jkbarson@gmail.com>
2017-10-02 21:47:57 -0400

    modify articles.js

--- routes/API/articles.js
+++ routes/API/articles.js
@@ -1,4 +1,16 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
+//Route
+router.route("/")
+  .get(articlesController.findAll)
+
 //Need /api/saved routes
+
+router.route("/api/saved")
+  .get(articlesController.findById)
+  .put(articlesController.update)
+
+
+router.route("/api/saved/:id")
+  .delete(articlesController.remove);
